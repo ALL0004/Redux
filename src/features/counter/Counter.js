0bc2e82161d7384d337606a5ea130be607ca67fb
Antonio Lopez Luque <all0004@alu.medac.es>
2022-02-09 18:44:35 +0100

    práctica Redux

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -4,18 +4,23 @@ import {
   decrement,
   increment,
   incrementByAmount,
+  sumAndSmite,
   incrementAsync,
   incrementIfOdd,
   selectCount,
 } from './counterSlice';
 import styles from './Counter.module.css';
+import { store } from '../../app/store';
 
 export function Counter() {
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
   const [incrementAmount, setIncrementAmount] = useState('2');
-
+  const [sum1Amount, setSum1Amount] = useState("0");
+  const [sum2Amount, setSum2Amount] = useState("0");
   const incrementValue = Number(incrementAmount) || 0;
+
+  const sumValue = Number(sum1Amount) + Number(sum2Amount) || 0;
 
   return (
     <div>
@@ -62,6 +67,34 @@ export function Counter() {
           Add If Odd
         </button>
       </div>
+      <div className={styles.row}>
+        <button
+          className={styles.button}
+          onClick={() => setSum1Amount(store.getState().counter.value)}
+        >
+          Carga contador  
+        </button>
+        <input
+            className={styles.textbox}
+            aria-label="Sumador 1"
+            value={sum1Amount}
+            onChange={(e) => setSum1Amount(e.target.value)}
+          />
+          <p>+</p>
+          <input
+            className={styles.textbox}
+            aria-label="Sumador 2"
+            value={sum2Amount}
+            onChange={(e) => setSum2Amount(e.target.value)}
+          />
+          <button
+          className={styles.button}
+          onClick={() => dispatch(sumAndSmite(sumValue))}
+        >
+          Suma y Machaca
+        </button>
+      </div>
+        
     </div>
   );
 }
